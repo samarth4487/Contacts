701d96bf684c0faa48f1dde80f5190302f94e53e
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2018-02-05 20:43:30 +0530

    basic contacts list added

--- Contacts/Controller/ContactsViewController.swift
+++ Contacts/Controller/ContactsViewController.swift
@@ -7,13 +7,90 @@
 //
 
 import UIKit
+import Alamofire
+import ObjectMapper
 
-class ContactsViewController: UIViewController {
+class ContactsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    //MARK: - Variables
+    
+    let contactsUrl = "http://jsonplaceholder.typicode.com/users"
+    var contacts: [Contact]?
+    var isDisplayingAscendingOrder = true
+    
+    
+    //MARK: - IBOutlets
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    //MARK: - View Controller Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .singleLine
+        fetchContactsData()
+    }
+    
+    
+    //MARK: - User Defined Mthods
+    
+    func fetchContactsData() {
         
+        Alamofire.request(contactsUrl, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil).responseJSON { (response) in
+            
+            if let data = response.result.value as? [NSDictionary] {
+                self.contacts = Mapper<Contact>().mapArray(JSONObject: data)
+                self.sortContactsIn(ascending: true)
+            }
+        }
+    }
+    
+    func sortContactsIn(ascending: Bool) {
+        
+        if ascending {
+            contacts = contacts?.sorted(by: { $0.name! < $1.name! })
+        } else {
+            contacts = contacts?.sorted(by: { $0.name! > $1.name! })
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    @IBAction func sortTapped(_ sender: UIBarButtonItem) {
+        
+        if isDisplayingAscendingOrder {
+            sortContactsIn(ascending: false)
+            isDisplayingAscendingOrder = false
+        } else {
+            sortContactsIn(ascending: true)
+            isDisplayingAscendingOrder = true
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    
+    //MARK: - Table View Delegate Methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let count = contacts?.count {
+            return count
+        }
+        
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.selectionStyle = .none
+        cell.textLabel?.text = contacts?[indexPath.row].name
+        cell.detailTextLabel?.text = contacts?[indexPath.row].phone
+        return cell
     }
 
 }
--- Contacts/Model/Contact.swift
+++ Contacts/Model/Contact.swift
@@ -7,8 +7,104 @@
 //
 
 import Foundation
+import ObjectMapper
 
-class Contact {
+class Contact: Mappable {
     
+    var id: Int?
+    var name: String?
+    var username: String?
+    var email: String?
+    var phone: String?
+    var website: String?
+    var address: Address?
+    var company: Company?
     
+    init() {
+        
+    }
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        
+        id          <- map["id"]
+        name        <- map["name"]
+        username    <- map["username"]
+        email       <- map["email"]
+        phone       <- map["phone"]
+        website     <- map["website"]
+        address     <- map["address"]
+        company     <- map["company"]
+    }
+}
+
+class Address: Mappable {
+    
+    var street: String?
+    var suite: String?
+    var city: String?
+    var zipcode: String?
+    var geo: Geo?
+    
+    init() {
+        
+    }
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        
+        street      <- map["street"]
+        suite       <- map["suite"]
+        city        <- map["city"]
+        zipcode     <- map["zipcode"]
+        geo         <- map["geo"]
+    }
+}
+
+class Geo: Mappable {
+    
+    var lat: String?
+    var lng: String?
+    
+    init() {
+        
+    }
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        
+        lat     <- map["lat"]
+        lng     <- map["lng"]
+    }
+}
+
+class Company: Mappable {
+    
+    var name: String?
+    var catchPhrase: String?
+    var bs: String?
+    
+    init() {
+        
+    }
+    
+    required init?(map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        
+        name               <- map["name"]
+        catchPhrase         <- map["catchPhrase"]
+        bs                <- map["bs"]
+    }
 }
